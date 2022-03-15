60a194ee33eb0971d8b8000047c93b2d81fce80c
Nathan Webb <web19014@byui.edu>
2022-03-15 15:26:29 -0700

    Completed Section5

---
+++ section5/SimpleMenu/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SimpleMenu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please choose one of the following options");
+            Console.WriteLine("1 - Cappucino");
+            Console.WriteLine("2 - Latte");
+            Console.WriteLine("3 - Americano");
+            Console.WriteLine("4 - Mocha");
+            Console.WriteLine("5 - Macchiato");
+            Console.WriteLine("6 - Expresso");
+            Console.WriteLine("Q - Quit the program");
+            
+            char userChoice = ' ';
+            while (!(userChoice.Equals('q') || userChoice.Equals('Q')))
+            {
+                  userChoice = char.ToLower(Console.ReadKey(true).KeyChar);
+                  Console.WriteLine("You chose " + userChoice);
+            }
+            
+            
+        }
+    }
+}
