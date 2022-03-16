09845f3202b358d4a9aebb5fd28eacb0d5c30927
Nathan Webb <web19014@byui.edu>
2022-03-16 15:58:27 -0700

    Completed Section 6 and 7

---
+++ section6/Ternary/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Ternary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What day is it? ");
+            string day = Console.ReadLine();
+            string howToSpendTime;
+            
+            //if (day.ToLower().Equals("saturday"))
+            //{
+                //howToSpendTime = "Relax";
+            //}
+            //else
+            //{
+                //howToSpendTime = "Work";
+            //}
+            
+            howToSpendTime = day.ToLower().Equals("saturday") ? "Relax" : "Work";
+            
+            Console.WriteLine($"{howToSpendTime} on {day}");
+        }
+    }
+}
---
+++ section7/ClassesIntro/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ClassesIntro
+{
+    class Progam
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car("First Car");
+            Car anotherCar = new Car("Second Car");
+            
+            myCar.Accelerate(5);
+            myCar.Accelerate(30);
+            myCar.Brake(4);
+            
+            anotherCar.Brake(15);
+        }
+    }
+    
+    class Car
+    {
+        private int speed = 0;
+        private string name;
+        
+        public Car(string carName)
+        {
+            name = carName;
+        }
+        
+        public void Accelerate(int amount)
+        {
+            speed += amount;
+            ShowSpeed();
+        }
+        
+        public void Brake(int speedReduction)
+        {
+            speed -= speedReduction;
+            if(speed < 0)
+            {
+                speed = 0;
+            }
+            ShowSpeed();
+        }
+        
+        private void ShowSpeed()
+        {
+            Console.WriteLine($"{name} is going {speed} MPH.");
+        }
+    }
+}
